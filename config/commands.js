78dc46939c6c8cd9547dc15160f4b5c45af89c61
InPSServ <allenidicula@gmail.com>
2014-08-27 00:05:02 -0400

    Unblock Image

--- config/commands.js
+++ config/commands.js
@@ -304,7 +304,7 @@ var commands = exports.commands = {
 
 	gg: function(target, room, user) {
                     if(!this.canBroadcast()) return;
-                    this.sendReplyBox('<center><img src="http://s28.postimg.org/u3t9nqknd/OBAMA.jpg"><br />' +
+                    this.sendReplyBox('<center><img src="http://i.snag.gy/BiXq3.jpg"><br />' +
                                       '<font size=3><i><font color=red><b>Da Bic Boi</b></font></i></font><br />' +
                                       '<b><blink>Ace: Bullet Punch</blink></b><br />' +
                                       '<b>#hardbody</b>')
